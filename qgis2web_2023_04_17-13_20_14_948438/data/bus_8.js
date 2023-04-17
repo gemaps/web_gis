a85526cc9496f928f083e81f3540af49f2f516b6
geomaps <107353795+gemaps@users.noreply.github.com>
2023-04-17 13:24:17 +0300

    Add files via upload

---
+++ qgis2web_2023_04_17-13_20_14_948438/data/bus_8.js
@@ -0,0 +1,24 @@
+var json_bus_8 = {
+"type": "FeatureCollection",
+"name": "bus_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.114187744785539, 48.4098423532607 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.113882160486298, 48.409492201974373 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.119868203339905, 48.406989579523476 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.120838938588037, 48.406653491033708 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.133064204675264, 48.402749320065837 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.133038318401987, 48.402627822238045 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.132088734133333, 48.402841389159676 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.131798934147042, 48.40275898943068 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.12660526380489, 48.403284705805703 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.12638680891331, 48.403202096597397 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.11346292913359, 48.402166001707755 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.112959093863424, 48.402388819605136 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.116482783892451, 48.40087658150037 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.116542132909245, 48.400772316545023 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.120624587617726, 48.400298496882101 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.12066120722389, 48.400187924307737 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 135.126724277524744, 48.401736759178441 ] } }
+]
+}
---
+++ qgis2web_2023_04_17-13_20_14_948438/data/poi_9.js
@@ -0,0 +1,29 @@
+var json_poi_9 = {
+"type": "FeatureCollection",
+"name": "poi_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CodePOI": "12A", "Name": "Стоматологическая поликлиника №22" }, "geometry": { "type": "Point", "coordinates": [ 135.115555246791189, 48.39763920109845 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "12B", "Name": "Стоматологическая поликлиника Айсберг" }, "geometry": { "type": "Point", "coordinates": [ 135.124593411273622, 48.402244511049879 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "13A", "Name": "Краевой кожно-венерологический диспансер" }, "geometry": { "type": "Point", "coordinates": [ 135.114151004101899, 48.408811401099236 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "14A", "Name": "Аптека Ваше Здоровье" }, "geometry": { "type": "Point", "coordinates": [ 135.12025575410172, 48.406485701098667 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "14A", "Name": "Аптека Хабмедторг" }, "geometry": { "type": "Point", "coordinates": [ 135.124842037169657, 48.402158602961549 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "14A", "Name": "Новая аптека" }, "geometry": { "type": "Point", "coordinates": [ 135.128163330030475, 48.397587951097549 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "21A", "Name": "Школа №72" }, "geometry": { "type": "Point", "coordinates": [ 135.122358106545846, 48.404427351098377 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "21A", "Name": "Школа №52" }, "geometry": { "type": "Point", "coordinates": [ 135.114367304101904, 48.397602701098521 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "22B", "Name": "Школа-интернат №6" }, "geometry": { "type": "Point", "coordinates": [ 135.124828244344627, 48.406171701098295 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "23B", "Name": "Детский сад Доверие" }, "geometry": { "type": "Point", "coordinates": [ 135.120824759369071, 48.406501901098615 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "23B", "Name": "Детский сад Моя радость" }, "geometry": { "type": "Point", "coordinates": [ 135.121232232313446, 48.404346951098461 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "23A", "Name": "Детский сад №200" }, "geometry": { "type": "Point", "coordinates": [ 135.111738272367489, 48.397728351098756 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "23A", "Name": "Детский сад №49" }, "geometry": { "type": "Point", "coordinates": [ 135.116672473505361, 48.395985101098304 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "24A", "Name": "Центр развития творчества" }, "geometry": { "type": "Point", "coordinates": [ 135.126980145250741, 48.401956901097883 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "31A", "Name": "Завод Витязь" }, "geometry": { "type": "Point", "coordinates": [ 135.130015882129953, 48.402125001097666 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "31A", "Name": "Картонно-рубероидный завод" }, "geometry": { "type": "Point", "coordinates": [ 135.130007905283236, 48.403040251097728 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "41A", "Name": "Пожарная часть №30" }, "geometry": { "type": "Point", "coordinates": [ 135.115640787945182, 48.41071955109927 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "42A", "Name": "Пункт полиции" }, "geometry": { "type": "Point", "coordinates": [ 135.126211595423797, 48.402186401097957 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "43A", "Name": "Сбербанк" }, "geometry": { "type": "Point", "coordinates": [ 135.12124160410167, 48.404333001098465 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "44A", "Name": "Почта России" }, "geometry": { "type": "Point", "coordinates": [ 135.119217454101715, 48.40658410109873 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "51A", "Name": "ДКиС Юность" }, "geometry": { "type": "Point", "coordinates": [ 135.12093262551528, 48.403853651098451 ] } },
+{ "type": "Feature", "properties": { "CodePOI": "51A", "Name": "ДК ТЭЦ-1" }, "geometry": { "type": "Point", "coordinates": [ 135.113446975961153, 48.409866101099361 ] } }
+]
+}
